b2c9a5c349769f11401b2f0f7ca0642f00b45516
Luis Henrique da Silva Campos <luis.henrique_campos@outlook.com.br>
2024-01-22 16:29:55 -0300

    update campo and example queimadas

---
+++ Queimadas/project.lua
@@ -0,0 +1,12 @@
+
+import("gis")
+
+project = Project{
+	title = "Coleta de Solos na Região de Queimadas",
+	author = "NEXUS",
+	file = "queimadas.tview",
+	clean = true,
+	trajectory = "trajectory_campina_grande_invert.shp",
+	points = "Pontos_campo_solos_abril_comunicacao.shp"
+}
+
